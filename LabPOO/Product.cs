204a65777022cb3f23630a617d6dffeccdea028c
macanepa <macanepa@miuandes.cl>
2018-05-08 17:12:12 -0300

    UPDATE 1

--- LabPOO/Product.cs
+++ LabPOO/Product.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace LabPOO
 {
-    class Product
+    public class ProductEventArgs : EventArgs
+    {
+        public Product product { get; set; }
+    }
+
+    public class Product
     {
         private string name;
         private int stock;
         private int price; //Price for one unit of the product
         private string unit;
+
+        //public event EventHandler<ProductEventArgs> AddedProduct;
+
+
+        //ES AQUI DONDE IMPLEMENTAMOS EL DELEGATE
+        public delegate void VideoEncodedEventHandler(object source, ProductEventArgs args);
+        public event VideoEncodedEventHandler AddedProduct;
+
 
         public Product(string name, int price, int stock, string unit)
         {
@@ -27,7 +40,13 @@ namespace LabPOO
             {
                 carrito.Add(this);
                 stock--;
+
+                OnAddedProduct(this);
+
                 return true;
+
+
+
             }
             return false;
         }
@@ -36,5 +55,19 @@ namespace LabPOO
         public int Stock { get => stock; }
         public int Price { get => price; }
         public string Unit { get => unit; }
+
+
+        protected virtual void OnAddedProduct(Product product)
+        {
+            if (AddedProduct != null)
+            {
+                AddedProduct(this, new ProductEventArgs() { product = this });
+            }
+        }
+
+
+
+
+
     }
 }
--- LabPOO/Program.cs
+++ LabPOO/Program.cs
@@ -7,16 +7,34 @@ using System.Threading.Tasks;
 
 namespace LabPOO
 {
-    class Program
+  
+
+    public class Program
     {
         public static List<Product> cart;
         public static List<Product> market;
+        public static List<Product> ProductsRecepie = new List<Product>();
+
+
+        static Gestor gestor = new Gestor();
+
 
         static void Main(string[] args)
         {
             cart = new List<Product>();
             market = new List<Product>();
             SupplyStore();
+            AddProductRecepie();
+
+            /*
+            foreach(Product product in ProductsRecepie)
+            {
+                Console.WriteLine(product.Name);
+            }
+            Console.ReadKey();
+            */
+
+
             while (true)
             {
                 PrintHeader();
@@ -46,6 +64,10 @@ namespace LabPOO
                     }
                     else if (answer == "4")
                     {
+
+                        gestor +=
+
+
                         Pay();
                         break;
                     }
@@ -117,7 +139,7 @@ namespace LabPOO
                 response = Console.ReadKey(true);
             }
         }
-
+        
         public static void PrintProduct(int index, Product product)
         {
             Console.WriteLine(String.Format("{0}. {1}\n\tPrecio: ${2}\t{3}\tStock: {4}\n", index.ToString(), product.Name, product.Price, product.Unit, product.Stock));
@@ -131,34 +153,35 @@ namespace LabPOO
 
         public static bool AddToCart(Product product)
         {
+            product.AddedProduct += gestor.OnAddedProduct;
             return product.Agregar(cart);
         }
 
         public static void SupplyStore()
         {
-            market.Add(new Product("Leche Entera", 820, 89, "1L"));
+            market.Add(new Product("Leche Entera", 820, 89, "1L"));// 500 ml    1
             market.Add(new Product("Gomitas Flipy", 720, 12, "100g"));
-            market.Add(new Product("Mantequilla", 850, 12, "125g"));
+            market.Add(new Product("Mantequilla", 850, 12, "125g"));//50 gr     1
             market.Add(new Product("Crema para hemorroides", 4990, 7, "300cc"));
-            market.Add(new Product("Pimienta", 430, 84, "15g"));
+            market.Add(new Product("Pimienta", 430, 84, "15g"));//              1
             market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc"));
-            market.Add(new Product("Sal Lobos", 330, 150, "1kg"));
+            market.Add(new Product("Sal Lobos", 330, 150, "1kg"));//            1
             market.Add(new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un"));
-            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
+            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));//  1
             market.Add(new Product("Tomate", 1290, 200, "1kg"));
-            market.Add(new Product("Harina", 890, 43, "1kg"));
+            market.Add(new Product("Harina", 890, 43, "1kg"));//50 gr           1
             market.Add(new Product("Audifonos Samsung", 5990, 40, "1un"));
             market.Add(new Product("Pisco Alto del Carmen", 5990, 120, "1L"));
-            market.Add(new Product("Carne Molida", 4390, 15, "500g"));
-            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
+            market.Add(new Product("Carne Molida", 4390, 15, "500g"));// 300 gr 1
+            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));//
             market.Add(new Product("Sal parrillera", 840, 50, "750g"));
             market.Add(new Product("Cable HDMI 1m", 3990, 25, "1un"));
-            market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
-            market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));
-            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
-            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));
+            market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));// 70gr     2
+            market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));//      1   
+            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));//    1
+            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));// 250gr    1
             market.Add(new Product("Queso Parmesano", 3790, 41, "200g"));
-            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
+            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));//       1
         }
 
         public static void ShowRecipe()
@@ -190,6 +213,56 @@ namespace LabPOO
             {
                 response = Console.ReadKey(true);
             }
+        }
+
+
+        public static void AddProductRecepie()
+        {
+            ProductsRecepie.Add(new Product("Leche Entera", 820, 89, "1L"));// 500 ml    1
+            ProductsRecepie.Add(new Product("Mantequilla", 850, 12, "125g"));//50 gr     1
+            ProductsRecepie.Add(new Product("Pimienta", 430, 84, "15g"));//  
+            ProductsRecepie.Add(new Product("Sal Lobos", 330, 150, "1kg"));// 
+            ProductsRecepie.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));//  1
+            ProductsRecepie.Add(new Product("Harina", 890, 43, "1kg"));//
+            ProductsRecepie.Add(new Product("Audifonos Samsung", 5990, 40, "1un"));
+            ProductsRecepie.Add(new Product("Carne Molida", 4390, 15, "500g"));
+            ProductsRecepie.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));//
+            ProductsRecepie.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
+            ProductsRecepie.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
+            ProductsRecepie.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));// 
+            ProductsRecepie.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));// 
+            ProductsRecepie.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));// 
+            ProductsRecepie.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));//       1
+        }
+
+
+    }
+
+    public class Gestor
+    {
+        public void OnAddedProduct(object sender, ProductEventArgs e)
+        {
+            Console.WriteLine(e.product.Name);
+
+            bool isOnList = false;
+            foreach(Product prod in Program.ProductsRecepie)
+            {
+                if (prod.Name == e.product.Name)
+                {
+                    isOnList = true;
+                    break;
+                }
+            }
+
+            if (!isOnList)
+            {
+                Console.WriteLine("Remover producto");
+                Program.cart.Remove(e.product);
+            }
+            /*Product productFind = Program.ProductsRecepie.Any(x => x.Name == e.product.Name);*/
+
+            
+            Console.ReadKey();
         }
     }
 }
